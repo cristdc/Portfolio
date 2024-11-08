f4815b0eb99a096db18a4bf046137d24df6ae80b
cristdc <cristinadiazcabello@gmail.com>
2024-11-08 16:51:41 +0100

    Update curriculum and contact form functionality

--- javascript/jsForm.js
+++ javascript/jsForm.js
@@ -1,13 +1,14 @@
-// Script básico para manejar el envío del formulario
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    // Obtener los valores del formulario
     const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+    const asunto = document.getElementById('asunto').value;
     const message = document.getElementById('message').value;
 
-    // Validar los campos o enviar los datos a un servidor
-    alert(`Gracias ${name}, hemos recibido tu mensaje.`);
+    const email = "cristdc05@gmail.com";
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(asunto)}&body=${encodeURIComponent(message + "\n\nAtentamente,\n" + name)}`;
 
+    window.location.href = mailtoLink;
+
+    alert(`Gracias ${name}, hemos recibido tu mensaje.`);
 });
